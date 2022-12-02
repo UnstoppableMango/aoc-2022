3bafd9c1c7eb84310e45cb00fe8317fa3c204a64
UnstoppableMango <ecr.referee@hotmail.com>
2022-12-01 20:08:49 -0600

    Day1 haskell, rust and refactor F# (#4)

--- Day01/fsharp/Program.fs
+++ Day01/fsharp/Program.fs
@@ -1,42 +1,26 @@
 ﻿open System
-open System.Collections.Generic
 open System.IO
 
 let input = File.ReadLines "input.txt"
 
+let accumulate acc x =
+    match String.IsNullOrWhiteSpace x with
+    | true -> []::acc
+    | false -> (x::(List.head acc))::(List.tail acc)
+
 let part1 input =
-    let mutable curSum = List<int>()
-    let sums = List<List<int>>()
-
-    for i in input do
-        if String.IsNullOrWhiteSpace i then
-            sums.Add(curSum)
-            curSum <- List<int>()
-        else
-            curSum.Add(Int32.Parse(i))
-
-    let max = sums
-            |> Seq.map(fun x -> List.sum(List.ofSeq(x)))
-
-    List.max(List.ofSeq(max))
+    input
+    |> Seq.fold accumulate [[]]
+    |> Seq.map ((Seq.map Int32.Parse) >> Seq.sum)
+    |> Seq.max
 
 let part2 input =
-    let mutable group = List<int>()
-    let groups = List<List<int>>()
-
-    for i in input do
-        if String.IsNullOrWhiteSpace i then
-            groups.Add(group)
-            group <- List<int>()
-        else
-            group.Add(Int32.Parse(i))
-
-    let sums = groups
-            |> Seq.map(fun x -> List.sum(List.ofSeq(x)))
-            |> Seq.sortDescending
-            |> Seq.take 3
-
-    List.sum(List.ofSeq(sums))
+    input
+    |> Seq.fold accumulate [[]]
+    |> Seq.map ((Seq.map Int32.Parse) >> Seq.sum)
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
 
 part1 input |> Console.WriteLine
 part2 input |> Console.WriteLine
