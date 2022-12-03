f8dc02f4c8cee58e1f1a38815709d30f7007148d
UnstoppableMango <ecr.referee@hotmail.com>
2022-12-02 23:55:53 -0600

    Day 3 solution (#7)

--- Day03/fsharp/Program.fs
+++ Day03/fsharp/Program.fs
@@ -1,2 +1,34 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+
+let priority =
+    function
+    | x when Char.IsAsciiLetterLower(x) -> (int x) - 96
+    | x when Char.IsAsciiLetterUpper(x) -> (int x) - 38
+    | _ -> raise (InvalidOperationException())
+
+let part1 input =
+    input
+    |> Seq.map (fun x ->
+        x
+        |> Seq.splitInto 2
+        |> Seq.map Set
+        |> Set.intersectMany
+        |> Seq.exactlyOne
+        |> priority)
+    |> Seq.sum
+
+let part2 input =
+    input
+    |> Seq.chunkBySize 3
+    |> Seq.map (fun x ->
+        x
+        |> Seq.map Set
+        |> Set.intersectMany
+        |> Seq.exactlyOne
+        |> priority)
+    |> Seq.sum
+
+let input = File.ReadLines "input.txt"
+part1 input |> Console.WriteLine
+part2 input |> Console.WriteLine
