082aec6bb5a55b48a5632b76bb6c974cfe358b82
Erik Rasmussen <ecr.referee@hotmail.com>
2022-12-04 00:10:36 -0600

    Garbage day 4 solution

--- Day04/fsharp/Program.fs
+++ Day04/fsharp/Program.fs
@@ -1,2 +1,52 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+open System.Text.Json
+
+type SectionGroup = { Start: int
+                      End: int }
+
+type SectionIds = { First: SectionGroup
+                    Second: SectionGroup }
+
+type SectionGroup with
+    member x.IsSubset y =
+        Set.isSubset (seq { x.Start .. x.End } |> Set) (seq { y.Start .. y.End } |> Set)
+
+    member x.Intersect y =
+        Set.intersect (seq { x.Start .. x.End } |> Set) (seq { y.Start .. y.End } |> Set)
+
+let parseGroup (x: string) =
+    let split = x.Split('-')
+    let start = (Seq.head split) |> int
+    let last = (Seq.last split) |> int
+    { Start = start
+      End = last }
+
+let parse (x: string) =
+    let split = x.Split(',')
+    { First = parseGroup (Seq.head split)
+      Second = parseGroup (Seq.last split) }
+
+let solve1 (x: SectionIds) =
+    x.First.IsSubset x.Second || x.Second.IsSubset x.First
+
+let solve2 (x: SectionIds) =
+    (x.First.Intersect x.Second).Count > 0
+
+let part1 (input: string seq) =
+    input
+    |> Seq.map parse
+    |> Seq.map solve1
+    |> Seq.map (fun x -> if x then 1 else 0)
+    |> Seq.sum
+
+let part2 input =
+    input
+    |> Seq.map parse
+    |> Seq.map solve2
+    |> Seq.map (fun x -> if x then 1 else 0)
+    |> Seq.sum
+
+let input = File.ReadLines "input.txt"
+part1 input |> JsonSerializer.Serialize |> Console.WriteLine
+part2 input |> Console.WriteLine
