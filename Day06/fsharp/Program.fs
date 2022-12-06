feba638bb749ab459818e903bdd0beace8f444a3
UnstoppableMango <ecr.referee@hotmail.com>
2022-12-05 23:40:30 -0600

    Day 6 solution (#10)

--- Day06/fsharp/Program.fs
+++ Day06/fsharp/Program.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+
+let solve size (input: string) =
+    input
+    |> Seq.windowed size
+    |> Seq.map Set
+    |> Seq.findIndex (fun x -> x.Count = size)
+    |> fun x -> x + size
+
+let part1 = solve 4
+let part2 = solve 14
+
+let input = File.ReadLines "input.txt"
+input |> Seq.exactlyOne |> part1 |> Console.WriteLine
+input |> Seq.exactlyOne |> part2 |> Console.WriteLine
