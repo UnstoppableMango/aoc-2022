dedccfc1a3f55ca57b4b626693288440a76ac1ac
Erik Rasmussen <ecr.referee@hotmail.com>
2022-12-08 09:43:01 -0600

    WIP

--- Day08/fsharp/Program.fs
+++ Day08/fsharp/Program.fs
@@ -10,35 +10,73 @@ type Visibility =
     | Top
     | Bottom
 
-// type Visibilities = Map<int * int, Visibility list>
-//
-// let addVisibility (direction: Visibility) (shouldAdd: bool) (xs: Visibility list option) =
+type Visibilities = Map<int * int, Visibility list>
+
+// let addVisibility (shouldAdd: bool) (direction: Visibility) (xs: Visibility list option) =
 //     if shouldAdd then
 //         match xs with
 //         | Some l -> Some (direction :: l)
 //         | None -> Some [ direction ]
 //     else
 //         xs
-//
-// let visible (direction: Visibility) (row: int) (op: int -> int) (acc: Visibilities, tallest: int, col: int) (cur: int) =
-//     (acc.Change((row, col), (addVisibility direction (cur > tallest))), max cur tallest, op col)
-//
+
+let combineMap (acc: Visibilities) (row: seq<(int * int) * Visibility option>) : Visibilities =
+    let add v l =
+        match (v, l) with
+        | Some x, Some xs -> Some(x :: xs)
+        | Some x, None -> Some [ x ]
+        | None, _ -> l
+
+    row |> Seq.fold (fun inner (i, v) -> inner.Change(i, add v)) acc
+
+// let visible (direction: Visibility) (acc: Visibilities, tallest: int) (index: int * int, cur: int) =
+//     (acc.Change(index, (addVisibility (cur > tallest) direction)), max cur tallest)
+
+let mapVisible (direction: Visibility) (t: int) (i: int * int, c: int) =
+    let visibility = if c > t then Some direction else None
+    ((i, visibility), max c t)
+
 // let leftVisible = visible Left
 // let rightVisible = visible Right
 // let topVisible = visible Top
 // let bottomVisible = visible Bottom
 
-let mapVisible (direction: Visibility) (row: int seq) =
-    row |> Seq.mapFold (fun (col, tallest) cur ->
-        let v = if cur > tallest then Some direction else None
-        (v, (col + 1, max cur tallest))) (0, 0)
+// let mapVisible (direction: Visibility) (row: int seq) =
+//     row |> Seq.mapFold (fun (col, tallest) cur ->
+//         let v = if cur > tallest then Some direction else None
+//         (v, (col + 1, max cur tallest))) (0, 0)
+
+let solve (direction: Visibility) (visibilities: Visibilities) (input: seq<seq<(int * int) * int>>) =
+    input
+    |> Seq.map (Seq.mapFold (mapVisible direction) 0)
+    |> Seq.map fst
+    |> Seq.fold combineMap visibilities
 
 let part1 (input: int seq seq) =
-    let left = input |> Seq.map (mapVisible Left) |> Seq.map fst
-    let right = input |> Seq.map Seq.rev |> Seq.map (mapVisible Right) |> Seq.map fst
-    let bottom = input |> Seq.transpose |> Seq.map Seq.rev |> Seq.map ((mapVisible Bottom) >> fst) |> Seq.transpose
-    let top = input |> Seq.transpose |> Seq.map ((mapVisible Top) >> fst) |> Seq.transpose
-    left |> Seq.head |> Seq.zip (right |> Seq.head)
+    let indexed = input |> Seq.mapi (fun r -> Seq.mapi (fun c x -> ((r, c), x)))
+    let mutable visibilities: Visibilities = Map.empty
+    visibilities <- indexed |> solve Left visibilities
+    visibilities <- indexed |> Seq.map Seq.rev |> solve Right visibilities
+    visibilities <- indexed |> Seq.transpose |> solve Bottom visibilities
+    visibilities <- indexed |> Seq.transpose |> Seq.map Seq.rev |> solve Top visibilities
+
+    visibilities
+    |> Map.map (fun k v ->
+        match k with
+        | 0, _ -> true
+        | _, 0 -> true
+        | _ -> v.Length > 0)
+    |> Map.values
+    |> Seq.sumBy (fun x -> if x then 1 else 0)
+// --------------------------------------------------------------
+// let left = indexed |> Seq.map (Seq.fold leftVisible (Map.empty, 0)) |> Seq.map fst |> Seq.reduce combineMap
+// left
+// --------------------------------------------------------------
+// let left = input |> Seq.map (mapVisible Left) |> Seq.map fst
+// let right = input |> Seq.map Seq.rev |> Seq.map (mapVisible Right) |> Seq.map fst
+// let bottom = input |> Seq.transpose |> Seq.map Seq.rev |> Seq.map ((mapVisible Bottom) >> fst) |> Seq.transpose
+// let top = input |> Seq.transpose |> Seq.map ((mapVisible Top) >> fst) |> Seq.transpose
+// left |> Seq.head |> Seq.zip (right |> Seq.head)
 
 let part2 input = 69
 
