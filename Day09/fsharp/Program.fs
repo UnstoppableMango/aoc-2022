fd47909d560650f3b3042f82eab434ed8b38365d
Erik Rasmussen <ecr.referee@hotmail.com>
2022-12-09 15:00:26 -0600

    Part 2

--- Day09/fsharp/Program.fs
+++ Day09/fsharp/Program.fs
@@ -1,6 +1,6 @@
 ﻿open System
 open System.IO
-open System.Text.Json
+open System.Threading
 
 type Direction =
     | Left of int
@@ -10,6 +10,33 @@ type Direction =
 
 type Knot = { Row: int; Col: int }
 let emptyKnot = { Row = 0; Col = 0 }
+
+let shouldPlot = false
+let shouldPlotSteps = false
+
+let plot (knots: Knot seq) =
+    let size = 22
+    let head = Seq.head knots
+    let graph = Array2D.create size size -1
+
+    knots
+    |> Seq.iteri (fun i c ->
+        let row = c.Row + ((size / 2) - head.Row)
+        let col = c.Col + ((size / 2) - head.Col)
+        if graph[row, col] = -1 then graph[row, col] <- i)
+
+    let toChar =
+        function
+        | -1 -> "."
+        | 0 -> "H"
+        | n -> $"{n}"
+
+    Console.Clear()
+
+    Seq.init size id
+    |> Seq.map (fun i -> graph[i, *] |> Array.map toChar)
+    |> Seq.rev
+    |> Seq.iter (fun x -> Console.WriteLine(String.Join("", x)))
 
 let parse (x: string) =
     let split = x.Split(' ')
@@ -34,19 +61,23 @@ let follow (head: Knot) (tail: Knot) =
 
     match (x, y) with
     | 1, 2
-    | 2, 1 ->
+    | 2, 1
+    | 2, 2 ->
         { Row = tail.Row + 1
           Col = tail.Col + 1 }
     | -1, 2
-    | -2, 1 ->
+    | -2, 1
+    | -2, 2 ->
         { Row = tail.Row + 1
           Col = tail.Col - 1 }
     | 1, -2
-    | 2, -1 ->
+    | 2, -1
+    | 2, -2 ->
         { Row = tail.Row - 1
           Col = tail.Col + 1 }
     | -2, -1
-    | -1, -2 ->
+    | -1, -2
+    | -2, -2 ->
         { Row = tail.Row - 1
           Col = tail.Col - 1 }
     | 0, 2 -> { tail with Row = tail.Row + 1 }
@@ -54,16 +85,35 @@ let follow (head: Knot) (tail: Knot) =
     | 2, 0 -> { tail with Col = tail.Col + 1 }
     | -2, 0 -> { tail with Col = tail.Col - 1 }
     | r, c when dontMove r && dontMove c -> tail
-    | _ ->
-        failwith
-            $"Unexpected. head: {head |> JsonSerializer.Serialize}, tail: {tail |> JsonSerializer.Serialize}, x: {x}, y: {y}"
+    | _ -> failwith $"Unexpected. head: [{head.Row}, {head.Col}], tail: [{tail.Row}, {tail.Col}], x: {x}, y: {y}"
 
 let stepOne (move: Knot -> Knot) (head: Knot) (tail: Knot) (visited: Map<Knot, int>) =
+    if shouldPlotSteps then plot [head; tail]
     let nh = move head
+    if shouldPlotSteps then plot [nh; tail]
     let nt = follow nh tail
+
+    if shouldPlot then
+        plot [nh; nt]
+        Thread.Sleep 50
 
     let nv =
         visited.Change(nt, (fun x -> x |> Option.orElse (Some 0) |> Option.map ((+) 1)))
+
+    (nh, nt, nv)
+
+let stepOneAll (move: Knot -> Knot) (head: Knot) (tail: Knot list) (visited: Map<Knot, int>) =
+    if shouldPlotSteps then plot (head :: tail)
+    let nh = move head
+    if shouldPlotSteps then plot (nh :: tail)
+    let nt = tail |> List.scan follow nh |> List.tail
+
+    if shouldPlot then
+        plot (nh :: nt)
+        Thread.Sleep 50
+
+    let nv =
+        visited.Change(nt |> List.last, (fun x -> x |> Option.orElse (Some 0) |> Option.map ((+) 1)))
 
     (nh, nt, nv)
 
@@ -74,13 +124,13 @@ let rec stepP1 (head: Knot, tail: Knot, visited: Map<Knot, int>) (direction: Dir
     | Up n when n <> 0 -> stepP1 (stepOne moveUp head tail visited) (Up(n - 1))
     | Down n when n <> 0 -> stepP1 (stepOne moveDown head tail visited) (Down(n - 1))
     | _ -> (head, tail, visited)
-    
-let rec stepP2 (head: Knot, tail: Knot seq, visited: Map<Knot, int>) (direction: Direction) =
+
+let rec stepP2 (head: Knot, tail: Knot list, visited: Map<Knot, int>) (direction: Direction) =
     match direction with
-    | Left n when n <> 0 -> stepP2 (stepOne moveLeft head tail visited) (Left(n - 1))
-    | Right n when n <> 0 -> stepP2 (stepOne moveRight head tail visited) (Right(n - 1))
-    | Up n when n <> 0 -> stepP2 (stepOne moveUp head tail visited) (Up(n - 1))
-    | Down n when n <> 0 -> stepP2 (stepOne moveDown head tail visited) (Down(n - 1))
+    | Left n when n <> 0 -> stepP2 (stepOneAll moveLeft head tail visited) (Left(n - 1))
+    | Right n when n <> 0 -> stepP2 (stepOneAll moveRight head tail visited) (Right(n - 1))
+    | Up n when n <> 0 -> stepP2 (stepOneAll moveUp head tail visited) (Up(n - 1))
+    | Down n when n <> 0 -> stepP2 (stepOneAll moveDown head tail visited) (Down(n - 1))
     | _ -> (head, tail, visited)
 
 let solve folder state =
@@ -92,7 +142,7 @@ let solve folder state =
 
 let part1 = solve stepP1 (emptyKnot, emptyKnot, Map.empty)
 
-let part2 = solve stepP2 (emptyKnot, Seq.replicate 9 emptyKnot, Map.empty)
+let part2 = solve stepP2 (emptyKnot, List.replicate 9 emptyKnot, Map.empty)
 
 let input = File.ReadLines "input.txt"
 input |> Seq.map parse |> part1 |> Console.WriteLine
