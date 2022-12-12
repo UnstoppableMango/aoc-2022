6073292877f7989d9720fba3a163bbab89f9b091
Erik Rasmussen <ecr.referee@hotmail.com>
2022-12-12 01:19:30 -0600

    WIP

--- Day12/fsharp/Program.fs
+++ Day12/fsharp/Program.fs
@@ -1,2 +1,34 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+open System.Text.Json
+
+type Index = int * int
+
+type Node =
+    | Terrain of Index * int
+    | Start of Index * int
+    | End of Index * int
+
+let height = int >> (fun x -> x - 97)
+
+let parse =
+    Seq.mapi (fun r ->
+        (Seq.mapi (fun c ->
+            function
+            | 'S' -> Start((r, c), 0)
+            | 'E' -> End((r, c), Int32.MaxValue)
+            | n -> Terrain((r, c), (height n)))))
+
+let part1 input = input
+
+let part2 input = 69
+
+let input = File.ReadLines "input.txt" |> parse
+
+input
+|> part1
+|> JsonSerializer.Serialize
+|> (fun (x: string) -> x.Replace("],", "],\n"))
+|> Console.WriteLine
+
+input |> part2 |> Console.WriteLine
