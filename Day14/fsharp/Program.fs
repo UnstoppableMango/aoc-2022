69354bc1367dc1461632aaff81b83732fbaba44c
Erik Rasmussen <ecr.referee@hotmail.com>
2022-12-15 22:05:56 -0600

    WIP

--- Day14/fsharp/Program.fs
+++ Day14/fsharp/Program.fs
@@ -4,40 +4,50 @@ open System.Text.Json
 
 type Index = int * int
 
-let numRows = 100
+let numRows = 180
 let numCols = 200
 let numCells = numRows * numCols
-let toRow i = i / numRows
-let toCol i = i % numCols
+let toRow i = i / numCols
+let toCol i = i % numRows
 
-let shouldPlot (x: int, y: int) ((sx: int, sy: int), (ex: int, ey: int)) =
+let isRock (r: int, c: int) ((sx: int, sy: int), (ex: int, ey: int)) =
     if sx <> ex && sy <> ey then
         failwith "Rocks can be diagonal"
 
-    let plotX = sx < x && x < ex
-    let plotY = sy < y && y < ey
+    let plotCol = (min sx ex) <= c && c <= (max sx ex)
+    let plotRow = (min sy ey) <= r && r <= (max sy ey)
 
-    plotX || plotY
+    plotCol && plotRow
+
+let scale ((sx: int, sy: int), (ex: int, ey: int)) =
+    let scaleX x = (x - 500) + (numCols / 2)
+
+    ((scaleX sx, sy), (scaleX ex, ey))
+
+let shouldPlot point line = isRock point (scale line)
 
 let graph (routes: (Index * Index)[] seq) =
     let folder (a: Map<Index, char>) (i: int) =
         let key = (toRow i, toCol i)
 
-        if routes |> Seq.exists (Seq.exists (shouldPlot key)) then
-            a |> Map.add key '#'
-        else
-            a
+        let t = if key = (0, numCols / 2)
+                then 'O'
+                else if routes |> Seq.exists (Array.exists (shouldPlot key))
+                then '#'
+                else '.'
 
-    Seq.fold folder Map.empty (seq { 0 .. numCells })
+        Map.add key t a
+
+    Seq.fold folder Map.empty (seq { 0 .. (numCells - 1) })
 
 let plot (graph: Map<Index, char>) =
     Console.Clear()
 
-    for r in 0 .. numRows do
-        for c in 0 .. numCols do
+    for r in 0 .. (numRows - 1) do
+        for c in 0 .. (numCols - 1) do
             graph
             |> Map.tryFind (r, c)
-            |> Option.defaultValue '.'
+            |> Option.defaultValue 'X'
             |> Console.Write
         Console.WriteLine()
 
@@ -45,26 +55,26 @@ let parse =
     Seq.map (fun (l: string) ->
         l.Split(" -> ")
         |> Array.map (fun x -> x.Split(',') |> Array.map Int32.Parse)
-        |> Array.map (fun x -> (Array.head x, Array.last x))
-        |> Array.map (fun (r, c) -> (r - 250, c - 50)))
+        |> Array.map (fun x -> (Array.head x, Array.last x)))
     >> Seq.map Array.pairwise<Index>
 
 let part1 input =
-    let graph = graph input
-    plot graph
+    input
+    |> graph
+    |> plot
 
 let part2 = id
 
-let input = File.ReadLines "input.txt" |> parse |> Seq.take 1
-input |> part1 |> Console.WriteLine
-input |> part2 |> Console.WriteLine
+let input = File.ReadLines "input.txt" |> parse
+// input |> part1 |> Console.WriteLine
+// input |> part2 |> Console.WriteLine
 
 
-// let numRows2 = 5
-// let numCols2 = 5
-//
-// let toRow i = i / numRows2
-// let toCol i = i % numCols2
-//
-// let tests = [4; 6; 10; 11; 12; 23; 25]
-// tests |> Seq.iter (fun x -> Console.WriteLine $"i: {x}, ({toRow x}, {toCol x})")
+let numRows2 = 10
+let numCols2 = 5
+
+let toRow2 i = i / numCols2
+let toCol2 i = i % numRows2
+
+let tests = [4; 6; 10; 11; 12; 23; 25]
+tests |> Seq.iter (fun x -> Console.WriteLine $"i: {x}, ({toRow2 x}, {toCol2 x})")
